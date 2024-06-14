631eb5484b0911db862579cd6e5471d99fe3cd9a
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-06-14 10:19:32 +0900

    feat: 헤더 토글 기능 추가

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-(function () {})();
+// FUNCTION header 메뉴 버튼 클릭 시 수행
+const onClickMenuBtn = () => {
+  const menuBtn = document.querySelectorAll('.menu-close-btn');
+  const menu = document.querySelector('.menu');
+
+  // menuBtn.classList.toggle('');
+  menu.classList.toggle('menu--open');
+  menuBtn.forEach((el) => {
+    el.classList.toggle('menu-close-btn--open');
+  });
+};
+
+(function () {
+  const menuOpener = document.querySelector('.menu-opener');
+  const mbMenuBtn = document.querySelector('.menu-close-btn-container');
+  mbMenuBtn.addEventListener('click', onClickMenuBtn);
+  menuOpener.addEventListener('click', onClickMenuBtn);
+})();
