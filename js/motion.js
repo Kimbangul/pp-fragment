ee0eecf0ba7a0d07d4e8a93f53c8e8ab694b47b4
kimbangul <highcolor_12@naver.com>
2024-06-18 21:28:40 +0900

    feat: 메인 모션 수정

--- js/motion.js
+++ js/motion.js
@@ -1,13 +1,12 @@
 // PARAM selector
 const mainTextRoller = document.querySelectorAll('.main-title-roller');
 
-const textRollMotion = (selector, duration) => {
+const textRollMotion = (selector, duration, repeat = 4) => {
   const tl = gsap.timeline({
-    duration: duration,
     ease: 'ease.inOut',
   });
 
-  tl.fromTo(selector, { opacity: 0 }, { opacity: 1, duration: 1 });
+  tl.fromTo(selector, { opacity: 0 }, { opacity: 1 });
   selector.forEach((el, idx) => {
     console.log(el.children);
     tl.fromTo(
@@ -16,9 +15,10 @@ const textRollMotion = (selector, duration) => {
         yPercent: -100,
       },
       {
-        repeat: 6,
+        repeat: repeat,
         ease: 'none',
         yPercent: 0,
+        duration: duration / repeat,
         delay: idx % 2 === 0 ? 0.15 : 0,
       },
       'slide'
@@ -34,9 +34,10 @@ const introMotion = () => {
   const bgImg = gsap.utils.toArray('.main-bg-item img');
   const subText = gsap.utils.toArray('.main-sub-text');
   const bottomText = new SplitType('.main-bottom-text', {
-    types: 'lines',
+    types: 'lines, chars',
     tagName: 'span',
-    splitClass: 'line-wrap',
+    lineClass: 'line-wrap',
+    charClass: 'char',
   });
 
   const tl = gsap.timeline({
@@ -50,23 +51,33 @@ const introMotion = () => {
       opacity: 1,
       duration: 0.3,
       stagger: {
-        amount: 0.5,
+        amount: 1,
         from: 'random',
       },
     }
   );
-  tl.to(bgItem, { scale: 0.9, duration: 0.3 });
-  tl.to(bgImg, {
-    transform: `var(--move)`,
-    duration: 0.5,
-    ease: 'power4.inOut',
-    scale: 1,
-    onComplete: () => {
-      bgItem.forEach((el) => el.classList.add('float'));
+  tl.to(bgItem, { scale: 0.9, duration: 0.5 });
+  tl.to(
+    bgItem,
+    {
+      scale: 1,
     },
-  });
+    'move'
+  );
+  tl.to(
+    bgImg,
+    {
+      transform: () => `var(--move)`,
+      duration: 1.5,
+      ease: 'ease.inOut',
+      onComplete: () => {
+        bgItem.forEach((el) => el.classList.add('float'));
+      },
+    },
+    'move'
+  );
   // 텍스트 롤링 애니메이션
-  tl.add(textRollMotion(mainTextRoller, 3), '-=2');
+  tl.add(textRollMotion(mainTextRoller, 1.2), 'move-=0.1');
 
   // 서브텍스트 출력
   tl.fromTo(
@@ -75,7 +86,7 @@ const introMotion = () => {
     {
       opacity: 1,
       stagger: 0.05,
-      duration: 0.3,
+      duration: 0.7,
       onComplete: () => {
         bgItem.forEach((el) => el.classList.add('float'));
       },
@@ -96,10 +107,10 @@ const introMotion = () => {
   tl.fromTo(
     '.header-inner',
     {
-      yPercent: -100,
+      yPercent: () => -100,
     },
     {
-      yPercent: 0,
+      yPercent: () => 0,
       transition: 0.3,
     },
     'header'
@@ -107,14 +118,48 @@ const introMotion = () => {
   tl.fromTo(
     '.menu',
     {
-      xPercent: -100,
+      xPercent: () => -100,
     },
     {
-      xPercent: 0,
+      xPercent: () => 0,
       transition: 0.3,
     },
     'header'
   );
+
+  const firstLine = [bottomText.lines[0], bottomText.lines[2]];
+  const secondLine = [bottomText.lines[1], bottomText.lines[3]];
+  const line = [...firstLine, ...secondLine];
+
+  firstLine.forEach((el, idx) => {
+    tl.fromTo(
+      el.querySelectorAll('.char'),
+      {
+        yPercent: () => 100,
+      },
+      {
+        yPercent: () => 0,
+        duration: 0.2,
+      }
+    );
+  }, 'first-line');
+  secondLine.forEach((el, idx) => {
+    tl.fromTo(
+      el.querySelectorAll('.char'),
+      {
+        yPercent: () => 100,
+      },
+      {
+        yPercent: () => 0,
+        duration: 0.2,
+      }
+    );
+  }, 'second-line');
+
+  console.log(bottomText.chars);
+  // tl.fromTo(bottomText.lines, {
+
+  // })
 };
 
 (function () {
