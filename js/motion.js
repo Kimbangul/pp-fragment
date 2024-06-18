8cb354d754cc53f73ed3976206174b19dc6ae3f9
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-06-18 11:22:04 +0900

    feat: 모션 수정

--- js/motion.js
+++ js/motion.js
@@ -1,17 +1,21 @@
+// FUNCTION intro 글씨 깨지는 효과
 const introMotion = () => {
+  const bgItem = gsap.utils.toArray('.main-bg-item');
+  const bgImg = gsap.utils.toArray('.main-bg-item img');
+
   const tl = gsap.timeline({
     repeatRefresh: true,
     onComplete: () => {
-      console.log('complete');
+      bgItem.forEach((el) => el.classList.add('float'));
     },
   });
-  const bgItem = gsap.utils.toArray('.main-bg-item');
-  const bgImg = gsap.utils.toArray('.main-bg-item img');
 
   tl.to(bgItem, { scale: 0.9, duration: 0.2 });
   tl.to(bgItem, { scale: 1, delay: 1, duration: 0.2 });
   tl.to(bgImg, { transform: `var(--move)`, duration: 0.3 });
 };
+
+const textRollMotion = () => {};
 
 (function () {
   console.log('motion ready');
